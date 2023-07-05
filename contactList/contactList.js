a4b9a483c8f84394a98418d25b31b0fb5115017c
InsharaDev <138609068+InsharaDev@users.noreply.github.com>
2023-07-05 16:27:06 +0500

    Update contactList.js

--- contactList/contactList.js
+++ contactList/contactList.js
@@ -1,1 +1,34 @@
 
+import { LightningElement, wire, track } from 'lwc';
+import { refreshApex } from '@salesforce/apex';
+import getContacts from '@salesforce/apex/ContactController.getContacts';
+
+const columns = [
+    { label: 'First Name', fieldName: 'FirstName' },
+    { label: 'Last Name', fieldName: 'LastName' },
+    { label: 'Email', fieldName: 'Email' }
+];
+
+export default class ContactList extends LightningElement {
+    @track contacts = {
+        data: undefined,
+        error: undefined
+    };
+    columns = columns;
+
+    @wire(getContacts)
+    wiredContacts(result) {
+        const { data, error } = result;
+        if (data) {
+            this.contacts.data = data;
+            this.contacts.error = undefined;
+        } else if (error) {
+            this.contacts.error = error;
+            this.contacts.data = undefined;
+        }
+    }
+
+    refreshData() {
+        return refreshApex(this.wiredContacts);
+    }
+}
